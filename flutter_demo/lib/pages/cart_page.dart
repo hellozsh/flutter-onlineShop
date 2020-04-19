a8d0adb7b3fa89364fe1c57ac5c1be4ecfc53521
hello_zsh <hello_zsh@163.com>
2020-04-19 23:09:43 +0800

    53课未完成

--- flutter_demo/lib/pages/cart_page.dart
+++ flutter_demo/lib/pages/cart_page.dart
@@ -3,65 +3,85 @@ import './simpProvide/CartNumPage.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class CartPage extends StatefulWidget {
+class CartPage extends StatelessWidget {
 
-  _CartPageState createState() => _CartPageState();
-}
-
-class _CartPageState extends State<CartPage> {
-
-  List<String> testList = [];
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container(
-      child: Column(
-        children: <Widget>[
-          Container(
-            height: 500.0,
-            child: ListView.builder(
-                itemCount: testList.length,
-                itemBuilder: (context, index){
-                  return ListTile(
-                    title: Text(testList[index]),
-                  );
-                }
-            ),
-          ),
-          RaisedButton(
-            onPressed: (){_add();},
-            child: Text('增加'),
-          ),
-          RaisedButton(
-            onPressed: (){_clear();},
-            child: Text('清空'),
-          )
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('购物车'),
+      ),
+      body: FutureBuilder(
+          builder: (context, snapshot){
+
+          },
       ),
     );
   }
-
-  void _add() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String temp = "😂哈哈哈";
-    testList.add(temp);
-    prefs.setStringList('testInfo', testList);
-    _show();
-  }
-
-  void _show() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    setState(() {
-      if(prefs.getStringList('testInfo')!=null){
-        testList = prefs.getStringList('testInfo');
-      }
-    });
-  }
-  void _clear() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.remove('testInfo');
-    setState(() {
-      testList = [];
-    });
-  }
 }
+
+
+// 持久化示范
+//class CartPage extends StatefulWidget {
+//
+//  _CartPageState createState() => _CartPageState();
+//}
+//
+//class _CartPageState extends State<CartPage> {
+//
+//  List<String> testList = [];
+//  @override
+//  Widget build(BuildContext context) {
+//    // TODO: implement build
+//    return Container(
+//      child: Column(
+//        children: <Widget>[
+//          Container(
+//            height: 500.0,
+//            child: ListView.builder(
+//                itemCount: testList.length,
+//                itemBuilder: (context, index){
+//                  return ListTile(
+//                    title: Text(testList[index]),
+//                  );
+//                }
+//            ),
+//          ),
+//          RaisedButton(
+//            onPressed: (){_add();},
+//            child: Text('增加'),
+//          ),
+//          RaisedButton(
+//            onPressed: (){_clear();},
+//            child: Text('清空'),
+//          )
+//        ],
+//      ),
+//    );
+//  }
+//
+//  void _add() async {
+//    SharedPreferences prefs = await SharedPreferences.getInstance();
+//    String temp = "😂哈哈哈";
+//    testList.add(temp);
+//    prefs.setStringList('testInfo', testList);
+//    _show();
+//  }
+//
+//  void _show() async {
+//    SharedPreferences prefs = await SharedPreferences.getInstance();
+//    setState(() {
+//      if(prefs.getStringList('testInfo')!=null){
+//        testList = prefs.getStringList('testInfo');
+//      }
+//    });
+//  }
+//  void _clear() async {
+//    SharedPreferences prefs = await SharedPreferences.getInstance();
+//    prefs.remove('testInfo');
+//    setState(() {
+//      testList = [];
+//    });
+//  }
+//}
--- flutter_demo/lib/provider/cart.dart
+++ flutter_demo/lib/provider/cart.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
+import 'package:flutter_demo/model/cartInfoModel.dart';
 
 class CartProvider with ChangeNotifier{
 
   String cartString = "[]";
+  List<CartInfoMode> cartList=[];
 
   save(goodsId, goodsName,count,price,images) async{
 
@@ -19,27 +21,47 @@ class CartProvider with ChangeNotifier{
     tempList.forEach((item){
       if(item['goodsId']==goodsId){
         tempList[ival]['count']=item['count']+1;
+        cartList[ival].count++;
         isHave = true;
       }
       ival++;
     });
     if(!isHave){
-      tempList.add({
+      Map<String, dynamic> newGoods={
         'goodsId':goodsId,
         'goodsName':goodsName,
         'count':count,
         'price':price,
         'images':images,
-      });
+      };
+      tempList.add(newGoods);
+      cartList.add(CartInfoMode.fromJson(newGoods));
     }
     cartString=json.encode(tempList).toString();
     print(cartString);
+    print(cartList.toString());
     prefs.setString('cartInfo', cartString);
+    notifyListeners();
   }
 
   remove() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.remove('cartInfo');
+    notifyListeners();
+  }
+
+  getCartInfo() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    cartString = prefs.getString('cartInfo');
+    cartList=[];
+    if(cartString==null){
+      cartList=[];
+    }else{
+      List<Map> tempList = (json.decode(cartString.toString()) as List).cast();
+      tempList.forEach((item){
+        cartList.add(CartInfoMode.fromJson(item));
+      });
+    }
     notifyListeners();
   }
 }
